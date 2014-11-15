5011d7909aa1539b9377b888102a3c8f4c615f0c
Manfred Hantschel <thred@users.noreply.github.com>
2014-11-15 22:58:28 +0100

    added some more logos

--- src/main/java/io/github/thred/piconup/image/ImageIndex.java
+++ src/main/java/io/github/thred/piconup/image/ImageIndex.java
@@ -11,15 +11,18 @@ public class ImageIndex
 {
 
     private static final String[] PREDEFINED_NAMES = {"13th_street.png", "1tvrus.png", "3sat.png", "al_jazeera.png",
-        "alpendorf_tv.png", "anixe.png", "ard_alpha.png", "arte.png", "astro_tv.png", "atv.png", "atv2.png",
-        "atv_avrupa.png", "axn.png", "bbc_entertainment.png", "bbc_world.png", "beate_uhse_tv.png", "bibel_tv.png",
+        "alpendorf_tv.png", "anixe.png", "ard_alpha.png", "arte.png", "astro_tv.png", "atv2.png", "atv_avrupa.png",
+        "atv.png", "axn.png", "bbc_entertainment.png", "bbc_world.png", "beate_uhse_tv.png", "bibel_tv.png",
         "bloomberg_tv.png", "blue_movie.png", "bn_music.png", "bn_sat.png", "br.png", "bvn.png", "cnn_int.png",
         "das_erste.png", "deluxe_music.png", "discovery_channel.png", "disney_channel.png", "disney_cinematic.png",
-        "disney_junior.png", "disney_xd.png", "dm_sat.png", "dmax.png", "eins_festival.png", "einsplus.png", "hr.png",
-        "kabel_1.png", "kika.png", "mdr.png", "ndr.png", "orf_1.png", "orf_2.png", "orf_3.png", "orf_sport.png",
-        "phoenix.png", "pro7.png", "pro7maxx.png", "puls4.png", "rbb.png", "rtl.png", "rtl2.png", "rtlnitro.png",
-        "sat1.png", "sat1gold.png", "servustv.png", "sixx.png", "srf1.png", "srf2.png", "super_rtl.png", "swr.png",
-        "tele5.png", "viva.png", "vox.png", "wdr.png", "zdf.png", "zdf_info.png", "zdf_kultur.png", "zdf_neo.png"};
+        "disney_junior.png", "disney_xd.png", "dmax.png", "dm_sat.png", "duna_tv.png", "dw_tv.png",
+        "eins_festival.png", "einsplus.png", "euro_d.png", "euronews.png", "eurosport.png", "eurostar.png", "ewtn.png",
+        "fashionone.png", "fox.png", "fox_series.png", "frace_24.png", "fs1salzburg.png", "goldstar_tv.png",
+        "gotv.png", "heimatkanal.png", "history.png", "hitradio_oe3.png", "hr.png", "hse24.png", "kabel_1.png",
+        "kika.png", "mdr.png", "ndr.png", "orf_1.png", "orf_2.png", "orf_3.png", "orf_sport.png", "phoenix.png",
+        "pro7maxx.png", "pro7.png", "puls4.png", "rbb.png", "rtl2.png", "rtlnitro.png", "rtl.png", "sat1gold.png",
+        "sat1.png", "servustv.png", "sixx.png", "srf1.png", "srf2.png", "super_rtl.png", "swr.png", "tele5.png",
+        "viva.png", "vox.png", "wdr.png", "zdf_info.png", "zdf_kultur.png", "zdf_neo.png", "zdf.png"};
 
     public static ImageIndex createPredefined()
     {
@@ -55,10 +58,10 @@ public class ImageIndex
 
         ImageIndexEntry result =
             entries
-            .stream()
-            .max(
-                Comparator.comparingDouble((entry) -> PiconUpUtil.computeMatchFactor(entry.getSimplifiedName(),
-                    simplifiedName))).get();
+                .stream()
+                .max(
+                    Comparator.comparingDouble((entry) -> PiconUpUtil.computeMatchFactor(entry.getSimplifiedName(),
+                        simplifiedName))).get();
 
         if ((result != null) && (PiconUpUtil.computeMatchFactor(result.getSimplifiedName(), simplifiedName) < 0.333))
         {
